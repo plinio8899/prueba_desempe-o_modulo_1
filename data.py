b4c1cd1bf304c326da10f34241ba0baf0ee3c9be
ChristianCode89 <plinio888999@gmail.com>
2026-03-30 08:43:12 -0500

    create a list students logic, create a search students logic, and functions for this operations

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ student_database = [
 
 def register_student(name, age, program, status):
     try:
-        student_id = len(student_database)+1
+        student_id = len(student_database)
         student_array = {
             "id" : student_id,
             "name" : name, 
@@ -24,3 +24,56 @@ def register_student(name, age, program, status):
     except:
         print("[x] error to register student")
         return False
+
+def list_students():
+    try:
+        if len(student_database) == 1:
+            print("[x] The database is empty.")
+        else:
+            print("----------Students list----------")
+            for i in student_database:
+                if i.get("name") == "example_name":
+                    pass
+                else:
+                    print(f"\nID: {i.get("id")} \nName: {i.get("name")} \nAge: {i.get("age")} \nProgram: {i.get("program")} \nStatus: {i.get("status")}")
+    except:
+        print("[x] error to list students")
+
+def search_student(key, student_info):
+    search_list = []
+    try:
+        if key == 1:
+            for item in student_database:
+                name = item.get("id")
+                if name == student_info:
+                    search_list.append(item)
+        elif key == 2:
+            for item in student_database:
+                name = item.get("name")
+                if name == student_info:
+                    search_list.append(item)
+        elif key == 3:
+            for item in student_database:
+                name = item.get("age")
+                if name == student_info:
+                    search_list.append(item)
+        elif key == 4:
+            for item in student_database:
+                name = item.get("program")
+                if name == student_info:
+                    search_list.append(item)
+        elif key == 5:
+            for item in student_database:
+                name = item.get("status")
+                if name == student_info:
+                    search_list.append(item)
+                    
+        print("----------Students list----------")          
+        for i in search_list:
+            if i.get("name") == "example_name":
+                pass
+            else:
+                print(f"\nID: {i.get("id")} \nName: {i.get("name")} \nAge: {i.get("age")} \nProgram: {i.get("program")} \nStatus: {i.get("status")}")
+        
+    except:
+        print("[x] Error to search students")
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ while inter:
     print("options:")
     print("1. Register student")
     print("2. Consult datebase")
-    print("3. Search student by id")
+    print("3. Search student")
     print("4. Update student information")
     print("5. Delete student")
     print("6. Exit")
@@ -29,12 +29,13 @@ while inter:
 
 
                     if status != 0 and status != 1:
-                        status = 1
+                        status = "inactive"
                         reg = data.register_student(name, age, program, status)
                         os.system("clear")
                         print("[?] The assigned state does not exist, we will assign the default inactive state.")
                         time.sleep(1)
                     else:
+                        status = "active"
                         reg = data.register_student(name, age, program, status)
 
                     if not reg:
@@ -48,13 +49,68 @@ while inter:
                     print("[x] Error: invalid data")
                     time.sleep(1)
         elif option == 2:
-            print("consult")
+            os.system("clear")
+            data.list_students()
+            input("\npress enter to continue...")
         elif option == 3:
-            print("search")
+            inter_search = True
+            while inter_search:
+                os.system("clear")
+                print("----------Search by----------")
+                print("1. ID")
+                print("2. Name")
+                print("3. Age")
+                print("4. Program")
+                print("5. Status")
+                option_search = int(input("Choice option -> "))
+                if option_search == 1:
+                    try:
+                        student_info = int(input("insert id -> "))
+                        os.system("clear")
+                        data.search_student(1, student_info)
+                        input("Press enter to continue")
+                    except:
+                        print("[x] Error: invalid id")
+                elif option_search == 2:
+                    try:
+                        student_info = input("insert name -> ")
+                        os.system("clear")
+                        data.search_student(2, student_info)
+                        input("Press enter to continue")
+                    except:
+                        print("[x] Error: invalid name")
+                elif option_search == 3:
+                    try:
+                        student_info = int(input("insert age -> "))
+                        os.system("clear")
+                        data.search_student(3, student_info)
+                        input("Press enter to continue")
+                    except:
+                        print("[x] Error: invalid age")
+                elif option_search == 4:
+                    try:
+                        student_info = input("insert program name -> ")
+                        os.system("clear")
+                        data.search_student(4, student_info)
+                        input("Press enter to continue")
+                    except:
+                        print("[x] Error: invalid program name")
+                elif option_search == 5:
+                    try:
+                        student_info = input("insert status (active or inactive) -> ")
+                        os.system("clear")
+                        data.search_student(5, student_info)
+                        input("Press enter to continue")
+                    except:
+                        print("[x] Error: invalid status")
+                else:
+                    print("[x] Error: invalid option")
         elif option == 4:
             print("update")
+            time.sleep(1)
         elif option == 5:
             print("delete")
+            time.sleep(1)
         elif option == 6:
             inter = False
         else:
